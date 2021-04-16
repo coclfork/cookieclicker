29c388ef8807a4cfd2f6b33ab9f1da5e3a9e6f65
coclfork <82670916+coclfork@users.noreply.github.com>
2021-04-16 12:57:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ storeOptions = {
         product_owned: 0,
         image_src: "img/cursor.svg",
         product_name: "please love me",
-        product_cps: 1000000000000,
+        product_cps: 0.1,
         product_price: 10
     },
     2: {
